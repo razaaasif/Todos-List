ec9ee66540aa8f664f247c2a327c280da7661a2f
razaaasif <aasifraza9123@gmail.com>
2021-06-17 15:55:11 +0530

    final push

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 export default function Footer() {
+    
     return (
-        <div>
-            
-        </div>
+        <footer className="bg-dark text-light py-3" >
+            <p className="text-center">Copyright  &copy; MyTodosList.com</p>
+
+        </footer>
     )
 }
